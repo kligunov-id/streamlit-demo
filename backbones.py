cd7db1d9436144111891ae06d82ab627affb8903
Kligunov <kligunov.id@phystech.edu>
2023-10-20 00:14:55 +0300

    Add slow engine

--- backbones.py
+++ backbones.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 
 class NaiveEngine:
@@ -21,11 +22,21 @@ class NumpyEngine:
     def __init__(self):
         pass
 
-    def calculate(eslf, array):
+    def calculate(self, array):
         return array.mean()
+
+class SlowEngine(NumpyEngine):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.sleep_time = 2
+
+    def calculate(self, array):
+        time.sleep(self.sleep_time)
+        return super().calculate(array)
 
 backbone_dict = {
     "naive": NaiveEngine,
     "sum": SumEngine,
     "np.mean": NumpyEngine, 
+    "slow": SlowEngine,
 }
